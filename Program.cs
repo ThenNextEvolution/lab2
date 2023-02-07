a2af3764d7343f310098edb63e705172bd2f577b
eyita <me@ME>
2023-02-06 22:31:40 -0500

    new

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UtilityLibraries;
+Console.WriteLine("Enter first of 2 numbers,then press enter: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second of 2 numbers and press enter: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number and press enter: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+
+//Eyitayo Akinjinmi CPS_3330_TECH_4982_Lab2 ");
+
+Console.WriteLine(product.mutiply(a,b));
+
+Console.WriteLine(product.sum(a,b,c));
+Console.WriteLine( String.Format("Provided intergers: {0} {2} and {1}, the total is {3}",a,b,c, (a+b+c)));
---
+++ lab2/lab2.cs
@@ -0,0 +1,10 @@
+// using System;
+// WriteLine("Enter first of 2 numbers,then press enter: ");
+// int a = (int)Console.Read();
+
+// WriteLine("Enter second of 2 numbers and press enter: ");
+// int b = (int)Console.Read();
+
+// //WriteLine("Enter value for 'a' and press enter: ");
+// Console.Read();
+// WriteLine( String.Format("Provided intergers: {0} and {1}, the total is {3}",a,b,(a*b)));
